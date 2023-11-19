57eea27f5af1b6f4a51e7aa0545f1da0e4eaa56f
Junot Augusto Patrial Cordeiro <junotpcordeiro@gmail.com>
2023-11-19 12:03:24 -0300

    updates
    Exercício feito

--- mundo-02/aula-1.py
+++ mundo-02/aula-1.py
@@ -1,10 +1,12 @@
 """
 Condições aninhadas.
-Condições aninhadas em Python significa colocar uma estrutura condicional dentro de outra. Você usa isso quando precisa verificar várias condições de maneira sequencial. Seguem exemplos desta aula prática abaixo.
+Condições aninhadas em Python significa colocar uma estrutura condicional dentro de outra. 
+Você usa isso quando precisa verificar várias condições de maneira sequencial. Seguem exemplos desta aula prática abaixo.
 
 Estrutura Condicional Composta:
 
-Uma estrutura condicional composta, em programação, refere-se a um bloco de código que é executado com base em uma condição específica. Em Python, a estrutura condicional composta geralmente é implementada usando a instrução if, seguida por um bloco de código indentado, e opcionalmente pelas instruções else e elif (abreviação de "else if").
+Uma estrutura condicional composta, em programação, refere-se a um bloco de código que é executado com base em uma condição específica. 
+Em Python, a estrutura condicional composta geralmente é implementada usando a instrução if, seguida por um bloco de código indentado, e opcionalmente pelas instruções else e elif (abreviação de "else if").
 
 """
 nome = str(input("Digite seu nome: ")).capitalize() #letra maiuscula para o caso do usuário escrever sem letra maiúscula.
@@ -17,3 +19,4 @@ elif nome in 'Angela Junot Lucas Renata André':
 else:
     print(f"{nome}, seu nome é normal.")
 print(f'Tenha um bom dia, {nome}!')
+
---
+++ mundo-02/exercicios/ex036.py
@@ -0,0 +1,22 @@
+# Escreva um programa para aprovar um empréstimo bancário para a compra de um imóvel.
+# O programa vai perguntar o valor do imóvel, o salário do comprador e em quantos anos ele vai pagar.
+# Calcule o valor da prestação mensal sabendo que ela não pode exceder 30% do salário do comprador, ou o empréstimo será negado.
+
+while True:
+    valorImovel = input("Digite o valor do imóvel em R$: ")
+    salario = input("Digite seu salário mensal em R$: ")
+    financiamento = input("Digite em quantos anos deseja pagar: ")
+    if valorImovel.isnumeric() and salario.isnumeric() and financiamento.isnumeric():
+        valorImovel = int(valorImovel)
+        salario = int(salario)
+        financiamento = int(financiamento)
+        break
+    else:
+        print('Digite apenas números inteiros nas opções. Não utilize pontos ou vírgulas.')
+
+anos = financiamento * 12
+prestacao = valorImovel / anos
+if prestacao > salario * 30 / 100:
+    print("Empréstimo NEGADO.")
+else:
+    print("Empréstimo APROVADO.")
